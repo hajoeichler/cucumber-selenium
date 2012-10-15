7a17f911e5c1f827e3dd60df697c6a202a395a0e
Hajo Eichler <hajo.eichler@commercetools.de>
2012-10-15 15:59:18 +0200

    Fix default dir for test-config.

--- support/modules/cucumberselenium/config.rb
+++ support/modules/cucumberselenium/config.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module CucumberSelenium::Config
   def self.load_config
     config_dir = ENV['TEST_CONFIG_DIR']
-    config_dir = "."
+    config_dir ||= "."
     @@test_config = YAML.load(File.read("#{config_dir}/test-config.yaml"))
   end
   def test_config
