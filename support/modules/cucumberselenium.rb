6b5e4c30309f29caf9346fcc3e39a63a973f65ce
Hajo Eichler <hajo.eichler@commercetools.de>
2013-02-01 16:39:47 +0100

    Add test context.

--- support/modules/cucumberselenium.rb
+++ support/modules/cucumberselenium.rb
@@ -4,6 +4,7 @@ module CucumberSelenium
 end
 
 require 'cucumberselenium/config'
+require 'cucumberselenium/context'
 require 'cucumberselenium/tools'
 require 'cucumberselenium/gmail'
 require 'cucumberselenium/headless'
